1c71d73c78174bb9cdc09e18c2ded0d24c88d05b
zhifeng ou <zhifengou@hotmail.com>
2018-09-06 15:16:31 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -25,7 +25,7 @@ def list(songs)
 end
   
 def play(songs)
-  # puts "Please enter a song name or number:"
+  puts "Please enter a song name or number:"
   input=gets.chomp
   boolean=false
   songs.each_with_index do |value,index|
