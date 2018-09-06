183d71305960e61c74336bf0560564b39b681a76
zhifeng ou <zhifengou@hotmail.com>
2018-09-06 15:07:58 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -26,7 +26,7 @@ end
   
 def play(songs)
   puts "Please enter a song name or number:"
-  input=gets.downcase.chomp
+  input=gets.chomp
   songs.each_with_index do |value,index|
     if input==value
     puts "Playing #{input}"
